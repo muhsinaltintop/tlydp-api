d9795e5200ea67ff6994ec7fa98124d45ffdce07
Becca <beccasullivan30@gmail.com>
2022-01-24 21:40:32 +0000

    add route to get ducks

--- routes/ducks.router.js
+++ routes/ducks.router.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { getDucks } = require("../controllers/ducks.controllers");
 
 const ducksRouter = express.Router();
+
+ducksRouter.route('/').get(getDucks);
 
 module.exports = ducksRouter;
