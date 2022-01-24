10aa3f233e470936d176b993dfe9c9057d8d1af6
Becca <beccasullivan30@gmail.com>
2022-01-24 15:56:29 +0000

    change to async / await

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,16 +1,13 @@
 const db = require("../connection.js");
 const format = require("pg-format");
 
-const seed = (data) => {
+const seed = async (data) => {
   const { duckData, userData } = data;
-  return db
-    .query("DROP TABLE IF EXISTS ducks")
-    .then(() => {
-      return db.query("DROP TABLE IF EXISTS users");
-    })
-    .then(() => {
-      return db.query(
-        `CREATE TABLE users (
+  await db
+    .query("DROP TABLE IF EXISTS ducks");
+  await db.query("DROP TABLE IF EXISTS users");
+  await db.query(
+    `CREATE TABLE users (
         user_id SERIAL PRIMARY KEY,
         user_name VARCHAR(50) NOT NULL,
         first_name VARCHAR(50) NOT NULL,
@@ -19,11 +16,9 @@ const seed = (data) => {
         email VARCHAR(50) NOT NULL,
         profile_pic VARCHAR(100) NOT NULL
     );`
-      );
-    })
-    .then(() => {
-      return db.query(
-        `CREATE TABLE ducks (
+  );
+  await db.query(
+    `CREATE TABLE ducks (
             duck_id SERIAL PRIMARY KEY,
             duck_name VARCHAR(50) NOT NULL,
             maker_id INT NOT NULL REFERENCES users(user_id),
@@ -36,21 +31,19 @@ const seed = (data) => {
             image VARCHAR(100),
             comments TEXT
         );`
-      );
-    })
-    .then(() => {
-      const formattedUsers = userData.map((user) => {
-        return [
-          user.user_name,
-          user.first_name,
-          user.last_name,
-          user.password,
-          user.email,
-          user.profile_pic,
-        ];
-      });
-      const usersQuery = format(
-        `INSERT INTO users 
+  );
+  const formattedUsers = userData.map((user) => {
+    return [
+      user.user_name,
+      user.first_name,
+      user.last_name,
+      user.password,
+      user.email,
+      user.profile_pic,
+    ];
+  });
+  const usersQuery = format(
+    `INSERT INTO users 
         (user_name, 
         first_name, 
         last_name, 
@@ -58,27 +51,25 @@ const seed = (data) => {
         email, 
         profile_pic)
         VALUES %L;`,
-        formattedUsers
-      );
-      return db.query(usersQuery);
-    })
-    .then(() => {
-      const formattedDucks = duckData.map((duck) => {
-        return [
-          duck.duck_name,
-          duck.maker_id,
-          duck.finder_id,
-          duck.location_placed_lat,
-          duck.location_placed_lng,
-          duck.location_found_lat,
-          duck.location_found_lng,
-          duck.clue,
-          duck.image,
-          duck.comments,
-        ];
-      });
-      const ducksQuery = format(
-        `INSERT INTO ducks 
+    formattedUsers
+  );
+  await db.query(usersQuery);
+  const formattedDucks = duckData.map((duck) => {
+    return [
+      duck.duck_name,
+      duck.maker_id,
+      duck.finder_id,
+      duck.location_placed_lat,
+      duck.location_placed_lng,
+      duck.location_found_lat,
+      duck.location_found_lng,
+      duck.clue,
+      duck.image,
+      duck.comments,
+    ];
+  });
+  const ducksQuery = format(
+    `INSERT INTO ducks 
         (duck_name,
         maker_id,
         finder_id,
@@ -90,10 +81,9 @@ const seed = (data) => {
         image,
         comments)
         VALUES %L;`,
-        formattedDucks
-      );
-      return db.query(ducksQuery);
-    });
+    formattedDucks
+  );
+  await db.query(ducksQuery);
 };
 
 module.exports = seed;
