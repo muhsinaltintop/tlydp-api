72a1de1cb5d6cedc0c1f0d5a144bf74c7ff3e44b
Becca <beccasullivan30@gmail.com>
2022-01-25 10:42:17 +0000

    200 test for maker_id query

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -31,6 +31,29 @@ describe("GET /api/ducks", () => {
       );
     });
   });
+  test("200: returns array of only ducks that have been made by a user when passed a maker_id query", async () => {
+    const {
+      body: { ducks },
+    } = await request(app).get("/api/ducks?maker_id=2").expect(200);
+    expect(ducks).toHaveLength(6);
+    ducks.forEach((duck) => {
+      expect(duck).toEqual(
+        expect.objectContaining({
+          duck_id: expect.any(Number),
+          duck_name: expect.any(String),
+          maker_id: 2,
+          finder_id: expect.toBeTypeOrNull("number"),
+          location_placed_lat: expect.any(Number),
+          location_placed_lng: expect.any(Number),
+          location_found_lat: expect.toBeTypeOrNull("number"),
+          location_found_lng: expect.toBeTypeOrNull("number"),
+          clue: expect.any(String),
+          image: expect.any(String),
+          comments: expect.toBeTypeOrNull("string"),
+        })
+      );
+    });
+  });
 });
 
 describe("GET /api/ducks/found", () => {
@@ -65,7 +88,17 @@ describe("GET /api/ducks/found", () => {
     ducks.forEach((duck) => {
       expect(duck).toEqual(
         expect.objectContaining({
+          duck_id: expect.any(Number),
+          duck_name: expect.any(String),
+          maker_id: expect.any(Number),
           finder_id: 3,
+          location_placed_lat: expect.any(Number),
+          location_placed_lng: expect.any(Number),
+          location_found_lat: expect.any(Number),
+          location_found_lng: expect.any(Number),
+          clue: expect.any(String),
+          image: expect.any(String),
+          comments: expect.any(String),
         })
       );
     });
