ab72ebfb2509b5e9a7c72eebba12121bfd7f6e82
Becca <beccasullivan30@gmail.com>
2022-01-25 13:56:58 +0000

    change aliased join tables to plurals

--- models/ducks.models.js
+++ models/ducks.models.js
@@ -2,25 +2,25 @@ const db = require("../db/connection");
 
 exports.selectDucks = async (maker_id) => {
   const queryStr = `
-  SELECT ducks.*, maker.user_name AS maker_name, finder.user_name AS finder_name
+  SELECT ducks.*, makers.user_name AS maker_name, finders.user_name AS finder_name
   FROM ducks
-  JOIN users AS maker ON ducks.maker_id = maker.user_id
-  LEFT JOIN users AS finder ON ducks.finder_id = finder.user_id
+  JOIN users AS makers ON ducks.maker_id = maker.user_id
+  LEFT JOIN users AS finders ON ducks.finder_id = finder.user_id
   ${maker_id ? "WHERE maker_id = $1" : ""};`;
 
   const queryValues = maker_id ? [maker_id] : null;
 
   const { rows } = await db.query(queryStr, queryValues);
-  console.log(rows);
+
   return rows;
 };
 
 exports.selectFoundDucks = async (finder_id) => {
   const queryStr = `
-  SELECT ducks.*, maker.user_name AS maker_name, finder.user_name AS finder_name
+  SELECT ducks.*, makers.user_name AS maker_name, finders.user_name AS finder_name
   FROM ducks
-  JOIN users AS maker ON ducks.maker_id = maker.user_id
-  JOIN users AS finder ON ducks.finder_id = finder.user_id
+  JOIN users AS makers ON ducks.maker_id = maker.user_id
+  JOIN users AS finders ON ducks.finder_id = finder.user_id
   WHERE finder_id ${finder_id ? "= $1" : "IS NOT NULL"};`;
 
   const queryValues = finder_id ? [finder_id] : null;
@@ -32,7 +32,7 @@ exports.selectFoundDucks = async (finder_id) => {
 
 exports.selectUnfoundDucks = async () => {
   const { rows } = await db.query(`
-  SELECT ducks.duck_name, ducks.finder_id, ducks.location_placed_lat, ducks.location_placed_lng, ducks.clue, users.user_name
+  SELECT ducks.duck_name, ducks.finder_id, ducks.location_placed_lat, ducks.location_placed_lng, ducks.clue, users.user_name AS maker_name
   FROM ducks
   JOIN users ON ducks.maker_id = users.user_id
   WHERE finder_id IS NULL;`);
