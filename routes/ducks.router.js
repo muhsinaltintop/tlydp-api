6078b0b3d5f1be42b3b42a99be813aa6ad578d69
Becca <beccasullivan30@gmail.com>
2022-01-25 10:15:16 +0000

    add route for unfound ducks

--- routes/ducks.router.js
+++ routes/ducks.router.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { getDucks, getFoundDucks } = require("../controllers/ducks.controllers");
+const { getDucks, getFoundDucks, getUnfoundDucks } = require("../controllers/ducks.controllers");
 
 const ducksRouter = express.Router();
 
 ducksRouter.route("/").get(getDucks);
 
 ducksRouter.route("/found").get(getFoundDucks);
+
+ducksRouter.route("/unfound").get(getUnfoundDucks);
+
 
 module.exports = ducksRouter;
