2e4dffdf0a84e2606ff6faefa0f0068b59b0f3c3
Becca <beccasullivan30@gmail.com>
2022-01-25 09:25:01 +0000

    create selectFoundDucks

--- models/ducks.models.js
+++ models/ducks.models.js
@@ -2,7 +2,15 @@ const db = require("../db/connection");
 
 exports.selectDucks = async () => {
   const { rows } = await db.query(`
-    SELECT * FROM ducks`);
+  SELECT * FROM ducks`);
 
+  return rows;
+};
+
+exports.selectFoundDucks = async () => {
+  const { rows } = await db.query(`
+  SELECT * FROM ducks
+  WHERE finder_id IS NOT NULL`);
+  
   return rows;
 };
