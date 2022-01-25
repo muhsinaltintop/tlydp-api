63261f46f7c10f566190f23d1a08b81fea040ef3
Becca <beccasullivan30@gmail.com>
2022-01-25 10:15:35 +0000

    create getUnfoundDucks

--- controllers/ducks.controllers.js
+++ controllers/ducks.controllers.js
@@ -1,13 +1,28 @@
-const { selectDucks, selectFoundDucks } = require("../models/ducks.models");
+const {
+  selectDucks,
+  selectFoundDucks,
+  selectUnfoundDucks,
+} = require("../models/ducks.models");
 
 exports.getDucks = async (req, res, next) => {
   const ducks = await selectDucks();
+
+  //finder query
+  //maker query
+  //found
+  //unfound
 
   res.status(200).send({ ducks });
 };
 
 exports.getFoundDucks = async (req, res, next) => {
   const ducks = await selectFoundDucks();
+
+  res.status(200).send({ ducks });
+};
+
+exports.getUnfoundDucks = async (req, res, next) => {
+  const ducks = await selectUnfoundDucks();
 
   res.status(200).send({ ducks });
 };
