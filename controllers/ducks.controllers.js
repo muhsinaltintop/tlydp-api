492f1a202db02b6d220dd713d239b2a159d723ea
Becca <beccasullivan30@gmail.com>
2022-01-25 09:24:47 +0000

    create getFoundDucks

--- controllers/ducks.controllers.js
+++ controllers/ducks.controllers.js
@@ -1,7 +1,13 @@
-const { selectDucks } = require("../models/ducks.models");
+const { selectDucks, selectFoundDucks } = require("../models/ducks.models");
 
 exports.getDucks = async (req, res, next) => {
   const ducks = await selectDucks();
+
+  res.status(200).send({ ducks });
+};
+
+exports.getFoundDucks = async (req, res, next) => {
+  const ducks = await selectFoundDucks();
 
   res.status(200).send({ ducks });
 };
