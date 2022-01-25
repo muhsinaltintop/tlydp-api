e392c7e88f1a9d7b20d87f4bd274457b0fafbec0
Becca <beccasullivan30@gmail.com>
2022-01-25 10:28:26 +0000

    add optional finder_id query

--- models/ducks.models.js
+++ models/ducks.models.js
@@ -7,20 +7,24 @@ exports.selectDucks = async () => {
   return rows;
 };
 
-exports.selectFoundDucks = async () => {
-  const { rows } = await db.query(`
+exports.selectFoundDucks = async (finder_id) => {
+  const queryStr = `
   SELECT * FROM ducks
-  WHERE finder_id IS NOT NULL;`);
+  WHERE finder_id ${finder_id ? "= $1" : "IS NOT NULL"};`;
 
+  const queryValues = finder_id ? [finder_id] : null;
+
+  const { rows } = await db.query(queryStr, queryValues);
+  
   return rows;
 };
 
 exports.selectUnfoundDucks = async () => {
-  const {rows} = await db.query(`
+  const { rows } = await db.query(`
   SELECT ducks.duck_name, ducks.finder_id, ducks.location_placed_lat, ducks.location_placed_lng, ducks.clue, users.user_name
   FROM ducks
   JOIN users ON ducks.maker_id = users.user_id
-  WHERE finder_id IS NULL;`)
+  WHERE finder_id IS NULL;`);
 
   return rows;
 };
