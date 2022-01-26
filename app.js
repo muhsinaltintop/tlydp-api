16d01b3400885fcef2dd0ce691c19cb49a74a262
Becca <beccasullivan30@gmail.com>
2022-01-26 14:39:02 +0000

    add error handling functions

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { handleCustomErrors, handleServerErrors } = require("./errors/errors");
 const apiRouter = require("./routes/api.router");
 
 const app = express();
@@ -6,5 +7,8 @@ const app = express();
 app.use(express.json());
 
 app.use("/api", apiRouter);
+
+app.use(handleCustomErrors);
+app.use(handleServerErrors);
 
 module.exports = app;
