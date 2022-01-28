bb3d746a85fa11c43b1e631e4250365b5e4a8ae0
Becca <beccasullivan30@gmail.com>
2022-01-28 11:32:26 +0000

    allow cors

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const apiRouter = require("./routes/api.router");
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
 app.use("/api", apiRouter);
+
+app.all("/*", (req, res) => {
+  res.status(404).send({ msg: "Path not found" });
+});
 
 app.use(handleCustomErrors);
 app.use(handleServerErrors);
