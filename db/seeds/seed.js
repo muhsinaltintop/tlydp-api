b02ab6d506fac602b1521aa2c5d9aae18fb609b4
ch3wyb01 <84191337+ch3wyb01@users.noreply.github.com>
2022-01-31 14:56:10 +0000

    Update seed.js

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -10,11 +10,11 @@ const seed = async (data) => {
     `CREATE TABLE users (
         user_id SERIAL PRIMARY KEY,
         user_name VARCHAR(50) NOT NULL,
-        first_name VARCHAR(50) NOT NULL,
-        last_name VARCHAR(50) NOT NULL,
+        first_name VARCHAR(50),
+        last_name VARCHAR(50),
         password VARCHAR(50) NOT NULL,
         email VARCHAR(50) NOT NULL,
-        profile_pic VARCHAR(100) NOT NULL
+        profile_pic VARCHAR(100)
     );`
   );
   await db.query(
